9a8a0caac4ab2c187cc0e39d94ab782c4354f16c
Raghavendra <115609188+Rraghavendra123@users.noreply.github.com>
2025-03-12 12:31:31 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "ec2_public_ip" {
-  value = aws_instance.web_server.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.mysql_db.endpoint
+  description = "Endpoint for the RDS instance"
+  value       = aws_db_instance.mysql_db.endpoint
 }
