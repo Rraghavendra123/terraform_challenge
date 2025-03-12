761225a5719b61eb76678a5664d7ff294274d100
Raghavendra <115609188+Rraghavendra123@users.noreply.github.com>
2025-03-12 12:24:19 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -85,9 +85,9 @@ resource "aws_db_instance" "mysql_db" {
   storage_type           = "gp2"
   engine                = "mysql"
   engine_version        = "8.0"
-  instance_class        = "db.t2.micro"
-  username             = "admin"
-  password             = "Terraform123!"  # Replace with a secure secret manager reference
+  instance_class        = var.db_instance_class
+  username             = var.
+  password             = var. # Replace with a secure secret manager reference
   publicly_accessible  = false
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
